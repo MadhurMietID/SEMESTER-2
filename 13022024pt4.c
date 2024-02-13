b85fb10b42734dfe04efde6aa75ff5abe5c0190a
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2024-02-13 11:53:10 +0530

    Add files via upload

---
+++ 13022024pt4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x=(10,20,30);
+    printf("x=%d");
+    return 0;
+}
+/*PS D:\84> cd "d:\84\" ; if ($?) { gcc 13022024pt3.c -o 13022024pt3 } ; if ($?) { .\13022024pt3 }
+x=4200768
+Garbage Value*/
